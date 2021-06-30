cdd93ae329b24de571113019d9f4ab91b0caea80
gihal <gihal83@gmail.com>
2021-07-01 00:02:40 +1200

    Override clone and added readResolve function

--- src/main/java/com/pingenerator/PinGenerator.java
+++ src/main/java/com/pingenerator/PinGenerator.java
@@ -1,12 +1,25 @@
 package com.pingenerator;
 
+import java.io.ObjectStreamException;
 import java.io.Serializable;
+import java.security.SecureRandom;
+import java.util.HashSet;
+import java.util.Set;
 
 public class PinGenerator implements Serializable, Cloneable {
 
     private static PinGenerator pinGenerator;
 
     private PinGenerator() {
+    }
+
+    private Object readResolve() throws ObjectStreamException {
+        return pinGenerator;
+    }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        throw new CloneNotSupportedException();
     }
 
     public static synchronized PinGenerator getInstance() {
